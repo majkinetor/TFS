6c3baf1ff324636055354a3c7c0575ef66d456fb
Miodrag Milic <miodrag.milic@gmail.com>
2016-04-14 16:20:06 +0200

    tags

--- Get-TFSBuilds.ps1
+++ Get-TFSBuilds.ps1
@@ -11,11 +11,15 @@ function Get-TFSBuilds {
         #Return raw data insted of the table
         [switch] $Raw,
         #Number of latest builds to return, by default 15.
-        [int] $First=15
+        [int] $First=15,        #TODO: Builds API supports $top
+        #Tags
+        [string[]]$Tags
     )
     check_credential
 
-    $uri = "$proj_uri/_apis/build/builds?api-version=" + $global:tfs.api_version
+    if ($Tags) { $tag_filter = "tagFilters=" + ($Tags -join ',') + '&' }
+
+    $uri = "$proj_uri/_apis/build/builds?$($tag_filter)api-version=" + $global:tfs.api_version
     Write-Verbose "URI: $uri"
 
     $params = @{ Uri = $uri; Method = 'Get'}
@@ -24,7 +28,9 @@ function Get-TFSBuilds {
 
     $props = 'buildNumber', 'result',
              @{ N='Start time'   ; E={ (get-date $_.startTime).ToString($time_format) }},
-             @{ N='Duration (m)' ; E={ [math]::round( ((get-date $_.finishTime) - (get-date $_.startTime)).TotalMinutes, 1)}}
+             @{ N='Duration (m)' ; E={ [math]::round( ((get-date $_.finishTime) - (get-date $_.startTime)).TotalMinutes, 1)}},
+             'tags'
+
 
     $r.value | select -Property $props -First $First | ft -au
 }
