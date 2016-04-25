eed8c5fda5d653aff5aff85f61febd0c4d456bc4
Miodrag Milic <miodrag.milic@gmail.com>
2016-04-25 15:58:47 +0200

    processes

---
+++ Get-TFSProcesses.ps1
@@ -0,0 +1,20 @@
+# Author: Miodrag Milic <miodrag.milic@gmail.com>
+# Last Change: 25-Apr-2016.
+
+<#
+.SYNOPSIS
+    Get the TFS processes
+#>
+function Get-TFSProcesses {
+    [CmdletBinding()]
+    param(
+    )
+    check_credential
+
+    $uri = "$collection_uri/_apis/process/processes?api-version=" + $global:tfs.api_version
+    Write-Verbose "URI: $uri"
+
+    $params = @{ Uri = $uri; Method = 'Get'}
+    $r = invoke_rest $params
+    $r.value
+}
